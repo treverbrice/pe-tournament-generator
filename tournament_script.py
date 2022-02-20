3165232ccbcb212d601c8db33a7a084444cb301c
treverbrice <treverbrice@gmail.com>
2022-02-19 23:39:42 -0800

    Added get_team_name() method fixed type error in get_num_teams()

--- tournament_script.py
+++ tournament_script.py
@@ -3,14 +3,21 @@ run this script directly to generate tournament matchups based on season wins
 '''
 
 def get_num_teams() -> int:
-    user_num_teams = input("Enter the number of teams in the tournament: ")
+    user_num_teams = int(input("Enter the number of teams in the tournament: "))
     if user_num_teams < 2:
         print("The minimum number of teams is 2, try again.")
         user_num_teams = get_num_teams()
     return user_num_teams
 
 def get_team_name(team_num: int) -> str:
-    pass
+    user_team_name = input(f"Enter the name for team #{team_num}: ")
+    if len(user_team_name) < 2:
+        print("Team names must have at least 2 characters, try again")
+        user_team_name = get_team_name(team_num)
+    if len(user_team_name.split(" ")) > 2:
+        print("Team names may have at most 2 words, try again.")
+        user_team_name = get_team_name(team_num)
+    return user_team_name
 
 def get_num_games() -> int:
     pass
