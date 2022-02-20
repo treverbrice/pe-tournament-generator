44e46bf95f8e63e649dc7f1210a2ac6d05c7e5d7
treverbrice <treverbrice@gmail.com>
2022-02-19 23:14:28 -0800

    Added program skeleton

---
+++ tournament_script.py
@@ -0,0 +1,27 @@
+'''
+run this script directly to generate tournament matchups based on season wins
+'''
+
+def get_num_teams() -> int:
+    pass
+
+def get_team_name(team_num: int) -> str:
+    pass
+
+def get_num_games() -> int:
+    pass
+
+def get_num_wins(team_name: str) -> int:
+    pass
+
+def generate_round(teams: dict) -> None:
+    pass
+
+# teams = {"name": wins, "name": wins}
+if __name__ == "__main__":
+    # get number of teams
+    # loop through and get team names
+    # get num of games
+    # loop through and get num of wins
+    # loop, waiting for all games, or user exit
+    # print next tournament round
